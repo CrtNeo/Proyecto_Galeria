8f07f8900c21445ba50819f05454d2f993197786
CrtNeo <cesarcris02@gmail.com>
2021-10-22 11:27:06 +0200

    v5 acabado

--- proyecto1/views/partials/nav.part.php
+++ proyecto1/views/partials/nav.part.php
@@ -23,12 +23,14 @@
               <li class="<?= (existeOpcionMenuActivaEnArray(["blog", "single_post"])? "active" : "");?> lien">
                   <a href="<?= (esOpcionMenuActiva("blog")? "#" : "/blog.php");?>"><i class="fa fa-file-text sr-icons"></i> Blog</a>
                 </li>
-              <li class="<?= (esOpcionMenuActiva("contact")? "active" : "");?>lien">
-                <a href="<?= (esOpcionMenuActiva("contact")? "#" : "/contact.php");?>"><i class="fa fa-phone-square sr-icons"></i> Contact</a>
+              <li class="<?= (esOpcionMenuActiva("contact")? "active" : "");?> lien">
+                <a href="<?= (esOpcionMenuActiva("contact")? "#" : "/contact.php");?>">
+                <i class="fa fa-phone-square sr-icons"></i> Contact</a>
               </li>
-              <li class="<?= (esOpcionMenuActiva("galeria")? "active" : "");?>lien">
-                <a href="<?= (esOpcionMenuActiva("galeria")? "#" : "/galeria.php");?>"><i class="fa fa-image sr-icons"></i> Galeria</a>
-              </li>
+              <li class="<?= (esOpcionMenuActiva("galeria")? "active" : "");?> lien">
+                <a href="<?= (esOpcionMenuActiva("galeria")? "#" : "/galeria.php");?>">
+                <i class="fa fa-image sr-icons"></i> Galeria</a>
+              </li>  
             </ul>
          </div>
      </div>
